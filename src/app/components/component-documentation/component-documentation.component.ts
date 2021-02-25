80d5cf6570cb8c52bc7ec7284355b1ae77f4b665
igatto2018 <gattoignazio@hotmail.com>
2021-02-25 20:03:49 +0100

    Loader V2 Component

--- src/app/components/component-documentation/component-documentation.component.ts
+++ src/app/components/component-documentation/component-documentation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AccordionItem } from '../accordion/accordion-item.interface';
+import { LoaderType } from '../loader/models/loader-type.enum';
 
 @Component({
   selector: 'app-component-documentation',
@@ -20,6 +21,7 @@ export class ComponentDocumentationComponent implements OnInit {
     },
   ];
   public progressValue = 25;
+  public loaderType = LoaderType.Loading;
 
   constructor() {}
 
--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { LoaderType } from './models/loader-type.enum';
 
 @Component({
   selector: 'app-loader',
@@ -7,8 +8,42 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class LoaderComponent implements OnInit {
   @Input() public isLoading = false;
+  @Input() public loaderType: LoaderType = LoaderType.Circular;
+  public LoaderType = LoaderType;
+
+  public get loadingText(): string {
+    return `${this.loading}${this.loadingPeriods}`;
+  }
+
+  private loading = 'Loading';
+  private loadingPeriods = '.\0\0';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.loaderType === LoaderType.Loading) {
+      this.updateLoaderPeriods();
+    }
+  }
+
+  private updateLoaderPeriods() {
+    let currentStep = 0;
+
+    setInterval(() => {
+      switch (currentStep % 3) {
+        case 0:
+          this.loadingPeriods = '..\0';
+          currentStep++;
+          break;
+        case 1:
+          this.loadingPeriods = '...';
+          currentStep++;
+          break;
+        case 2:
+          this.loadingPeriods = '.\0\0';
+          currentStep = 0;
+          break;
+      }
+    }, 500);
+  }
 }
