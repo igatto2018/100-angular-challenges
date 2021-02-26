e232760a0b086e6382e76552916c16a50568f9cd
igatto2018 <gattoignazio@hotmail.com>
2021-02-26 19:36:32 +0100

    FlattenPipe Pipes

--- src/app/pipes/pipe-documentation/pipe-documentation.component.ts
+++ src/app/pipes/pipe-documentation/pipe-documentation.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-pipe-documentation',
   templateUrl: './pipe-documentation.component.html',
-  styleUrls: ['./pipe-documentation.component.scss']
+  styleUrls: ['./pipe-documentation.component.scss'],
 })
 export class PipeDocumentationComponent implements OnInit {
+  public flattenData = [1, 2, [3], [4, [5, 6, [7]]]];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public modifyFlatten(): void {
+    this.flattenData.push(3, 5);
   }
 
+  public reassignFlatten(): void {
+    this.flattenData = [...this.flattenData];
+  }
 }
