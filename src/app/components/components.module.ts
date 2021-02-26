7e69649980c04af57ec08b532e666a3c9f3b92b0
igatto2018 <gattoignazio@hotmail.com>
2021-02-26 19:57:00 +0100

    Modal Component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -13,6 +13,7 @@ import { CreditCardInputComponent } from './credit-card-input/credit-card-input.
 import { FormsModule } from '@angular/forms';
 import { TwitterPostComponent } from './twitter-post/twitter-post.component';
 import { LinkedinPostComponent } from './linkedin-post/linkedin-post.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { LinkedinPostComponent } from './linkedin-post/linkedin-post.component';
     CreditCardInputComponent,
     TwitterPostComponent,
     LinkedinPostComponent,
+    ModalComponent,
   ],
   imports: [
     CommonModule,
